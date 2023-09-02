4ecaeaa3eb5dc64f2c10b3c42680e1692798730d
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-09-02 20:15:01 +0200

    openAI, C#, VS2022

---
+++ openAI/openAI/AI.cs
@@ -0,0 +1,36 @@
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
+using OpenAI_API.Models;
+using System.Reflection;
+
+List<ChatMessage> messages = new();
+messages.Add(new ChatMessage(ChatMessageRole.System, "Odpovídej v bodech"));
+Console.WriteLine("Nazdárek jsem tvá osobní AI, co tě zajímá?");
+
+
+while (true)
+{
+
+    string question = Console.ReadLine();
+    messages.Add(new ChatMessage(ChatMessageRole.User, question));
+
+
+    var ai = new OpenAIAPI(new APIAuthentication("Jsem vložíš API key", "Jsem vložíš Organization ID"));
+
+    ChatRequest request = new ChatRequest();
+    request.user = "Lukáš Kaňka";
+    request.Model = Model.ChatGPTTurbo;
+    request.Messages = messages;
+    request.Temperature = 0.7;
+    request.MaxTokens = 500;
+
+
+
+
+    var response = await ai.Chat.CreateChatCompletionAsync(request);
+    string answer = response.Choices.FirstOrDefault().Message.Content;
+    messages.Add(response.Choices.FirstOrDefault().Message);
+    Console.WriteLine(answer);
+}
+
+
